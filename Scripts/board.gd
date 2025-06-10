64e871619fe508234f50910e12eeee5a8db29e6f
Adam <sinclair.adamjohn@gmail.com>
2025-06-10 16:59:11 +1000

    feat: implement Board.create constructor

--- Scripts/board.gd
+++ Scripts/board.gd
@@ -2,8 +2,24 @@ extends Node2D
 
 class_name Board
 
+static var NUM_ROWS = 20
+static var NUM_COLS = 10
+
 static func create() -> Board:
-	return null
+	var board := Board.new()
+
+	# Setup the board with null representing no color/piece.
+	var c: Array[Array] = board.colors
+	c = []
+	c.resize(NUM_ROWS)
+
+	for i in range(NUM_ROWS):
+		c[i] = []
+		c[i].resize(NUM_COLS)
+		for j in range(NUM_COLS):
+			c[i][j] = null
+
+	return board
 
 func addPiece(piece: Piece):
 	return null
@@ -11,7 +27,5 @@ func addPiece(piece: Piece):
 func drop():
 	return null
 
-# Variables
-# 20 by 10
-
-var color := null
+# Colors has type Array[Array[Color]]
+var colors: Array[Array]
