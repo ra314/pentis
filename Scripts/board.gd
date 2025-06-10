2f8e481069e575665bc47879099b200066cf76be
Adam <sinclair.adamjohn@gmail.com>
2025-06-10 16:25:46 +1000

    feat: add board script skeleton

---
+++ Scripts/board.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+class_name Board
+
+static func create() -> Board:
+	return null
+
+func addPiece(piece: Piece):
+	return null
+
+func drop():
+	return null
+
+# Variables
+# 20 by 10
+
+var color := null
